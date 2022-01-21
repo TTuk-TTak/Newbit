c126594ca3cfc48c98c4365babe4b0faccc33c0b
이수림 <tnfla9689@naver.com>
2022-01-21 13:09:39 +0900

    initial commit Backend

---
+++ backend/Newbit/src/main/java/com/ssafy/newbit/NewbitApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.newbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewbitApplication.class, args);
+	}
+
+}
