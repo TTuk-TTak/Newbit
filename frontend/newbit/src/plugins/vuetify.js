bdc7cdae874409f42957fb000dd6c93d36fad2fb
sahngrhee <87463462+sahngrhee@users.noreply.github.com>
2022-01-25 11:17:53 +0900

    FEAT:�  로그인 페이지 구현 [#S06P12A101]

--- frontend/newbit/src/plugins/vuetify.js
+++ frontend/newbit/src/plugins/vuetify.js
@@ -4,4 +4,17 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    options: {
+      customProperties: true,
+    },
+    themes: {
+      light: {
+        background: '#E5E5E5'
+      },
+      dark: {
+
+      },
+    }
+  },
 });
