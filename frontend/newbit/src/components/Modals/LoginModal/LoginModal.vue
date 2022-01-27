b6843b312ba5714e393ac02933871adf9763214d
sahngrhee <87463462+sahngrhee@users.noreply.github.com>
2022-01-27 10:31:11 +0900

    FEAT:�  로그인 모달 레이아웃 구현 [#S06P12A101-20]

---
+++ frontend/newbit/src/components/Modals/LoginModal/LoginModal.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="text-center">
+    <v-dialog
+      v-model="dialog"
+      width="45%"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="red lighten-2"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          Click Me
+        </v-btn>
+      </template>
+      <v-card class="rounded-xl ma-0 pa-0">
+        <v-container>
+          <v-row>
+            <v-col
+              cols="6"
+              class="pa-0"
+            >
+              <v-img
+                src="@/assets/side.jpg"
+                class="rounded-l-xl"
+                height="100%"
+              ></v-img>
+            </v-col>
+            <v-col
+              cols="6"
+              class="pa-0"
+            >
+              <login-modal-default></login-modal-default>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+import LoginModalDefault from '@/components/Modals/LoginModal/LoginModalDefault.vue'
+
+
+export default {
+  components: { LoginModalDefault },
+
+  data () {
+    return {
+      dialog: false,
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+</style>
