921592b86f2bd45c45f547ac880cb7b3baad0fd0
Magpie1000 <web.1000c@gmail.com>
2022-02-01 23:21:32 +0900

    0201 local commit

---
+++ frontend/newbit/src/components/Commons/UserProfileIcon.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-avatar>
+    <img
+      :src="imgUrl"
+      alt="Img"
+    >
+  </v-avatar>
+</template>
+
+<script>
+export default {
+  name: 'UserProfileIcon',
+  props: {
+    imgUrl: String,
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/newbit/src/components/PostDetail/PostDetailReply.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card>
+    <user-profile-icon></user-profile-icon>
+  </v-card>
+</template>
+
+<script>
+import UserProfileIcon from '@/components/Commons/UserProfileIcon.vue'
+
+export default {
+  name: 'PostDetailReply',
+  components: {
+    UserProfileIcon,
+  },
+}
+</script>
+
+<style>
+
+</style>
