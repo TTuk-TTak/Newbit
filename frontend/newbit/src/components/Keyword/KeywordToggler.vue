b8d15e5513f98583aa27333403c24327a1efe99a
Magpie1000 <web.1000c@gmail.com>
2022-02-08 16:09:47 +0900

    CHORES: 오타 수정

--- frontend/newbit/src/components/Keyword/KeywordToggler.vue
+++ frontend/newbit/src/components/Keyword/KeywordToggler.vue
@@ -4,8 +4,8 @@
   >
     <keyword-chip
       v-for="(key, value) in keywords"
-      :key="key"
-      :text="value"
+      :key="value"
+      :text="key"
       :isInToggler='true'
     ></keyword-chip>
   </v-chip-group>
@@ -28,14 +28,8 @@ export default {
   computed: {
     // 키워드 中 소분류 키워드만 추출 후 반환.
     keywords: function () {
-      console.log(_.mapValues(this.$KEYWORDS, 'shownName'))
-      const keywordObject = {}
-      for (let keywordSets of Object.values(this.$KEYWORDS)) {
-        for (let [shownName, queryName] of Object.entries(keywordSets)) {
-          keywordObject[shownName] = queryName
-        }
-      }
-      return keywordObject
+      const keywords = _.mapValues(this.$KEYWORDS, 'shownName')
+      return keywords
     }
   },
   created: {
--- frontend/newbit/src/store/actions.js
+++ frontend/newbit/src/store/actions.js
@@ -4,6 +4,8 @@ import axios from 'axios'
 const serverURL = process.env.VUE_APP_SERVER_URL
 
 export default {
+  // 1. 모달 조작
+  // 1) PostCreateModal
   turnPostCreateModalOn: function ({commit}) {
     commit('TURN_POST_CREATE_MODAL_ON')
   },
@@ -11,6 +13,8 @@ export default {
     commit('TURN_POST_CREATE_MODAL_OFF')
   },
 
+  // 2. Feed조작
+  // 
   loadPosts: function () {
     const size = 8
     axios({
