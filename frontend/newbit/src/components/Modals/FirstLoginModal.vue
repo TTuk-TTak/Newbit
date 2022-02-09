26e5c9410cb0cbb3b2bde8657d4a98da633ec8df
이수림 <tnfla9689@naver.com>
2022-02-09 15:15:12 +0900

    FEAT: 🎸 관심키워드 선택 모달 UI 수정 [#S06P12A101-150]

--- frontend/newbit/src/components/Modals/FirstLoginModal.vue
+++ frontend/newbit/src/components/Modals/FirstLoginModal.vue
@@ -4,27 +4,27 @@
       v-model="dialog"
       width="700"
     >
-      <v-card class="rounded-xl ma-0 pa-0">
+      <v-card class="rounded-xl ma-0 pa-0 firstlogin pb-4">
         <v-container>
-          <v-card-title class="d-flex justify-center text-h5 font-weight-bold pb-0">
+          <v-card-title class="d-flex justify-center text-h5 font-weight-bold pb-0 modal-title">
             처음 오셨네요!
           </v-card-title>
 
           <v-card-text
             v-if="page === 1"
-            class="d-flex justify-center font-weight-bold pb-1"
+            class="d-flex justify-center font-weight-bold pb-1 modal-subtitle"
           >
             관심있는 키워드를 골라주세요!
           </v-card-text>
           <v-card-text
             v-else-if="page === 2"
-            class="d-flex justify-center font-weight-bold pb-1"
+            class="d-flex justify-center font-weight-bold pb-1 modal-subtitle"
           >
             관심사가 비슷한 사용자들을 팔로우 해보세요!
           </v-card-text>
           <v-card-text
             v-else-if="page === 3"
-            class="d-flex justify-center font-weight-bold pb-1"
+            class="d-flex justify-center font-weight-bold pb-1 modal-subtitle"
           >
             당신이 어떤분인지 저희에게 알려주세요!
           </v-card-text>
@@ -58,16 +58,17 @@
 
           <div
             v-if="page === 1 || page === 2"
-            class="text-center mb-3"
+            class="text-center mb-3 firstlogin"
           >
             <v-btn
               @click="nextPage"
               color="black"
               dark
+              depressed
               large
               rounded
-              width="40%"
-              class="font-weight-bold"
+              width="88%"
+              class="font-weight-bold btn"
             >다음으로
             </v-btn>
           </div>
@@ -79,10 +80,11 @@
               @click="dialog = false"
               color="black"
               dark
+              depressed
               large
               rounded
-              width="40%"
-              class="font-weight-bold mb-3"
+              width="88%"
+              class="font-weight-bold mb-3 btn"
             >입력 완료
             </v-btn>
           </div>
@@ -94,8 +96,8 @@
               @click="previousPage"
               large
               rounded
-              width="40%"
-              class="font-weight-bold"
+              width="88%"
+              class="font-weight-bold btn"
               outlined
             >이전으로
             </v-btn>
@@ -108,10 +110,10 @@
               @click="dialog = false"
               large
               rounded
-              width="40%"
-              class="font-weight-bold"
+              width="88%"
+              class="font-weight-bold btn"
               outlined
-            >다음에 할께요!
+            >다음에 할게요!
             </v-btn>
           </div>
         </v-container>
@@ -155,5 +157,36 @@ export default {
 }
 </script>
 
-<style>
+<style scope>
+/* 모달창 타이틀 (가장 큰 글씨) */
+.v-application .modal-title{
+  font-size : 2.0em !important;
+  margin-bottom: 0.3em;
+}
+
+/* 모달창 서브타이틀 (설명) */
+.v-application .modal-subtitle{
+  font-size : 1.4em !important;
+  margin-bottom: 1em;
+}
+
+/* 첫번째 모달 탭 메뉴 */
+.v-application .firstlogin .v-tab {
+  font-size: 1.2em;
+  font-weight: 300  !important;
+  color : #818181  !important;
+}
+
+/* 첫번째 모달 탭 메뉴(활성화) */
+.v-application .firstlogin .v-tab--active {
+  color : #0d0e23  !important;
+  font-weight: 600  !important;
+}
+
+/* 버튼 글씨 */
+.v-application .firstlogin .btn {
+  font-size: 1.3em;
+  font-weight: 500  !important;
+}
 </style>
+
--- frontend/newbit/src/plugins/vuetify.js
+++ frontend/newbit/src/plugins/vuetify.js
@@ -14,7 +14,7 @@ export default new Vuetify({
         backgroundDark: '#0D0E23',
         feedBackground: '#FFFFFF',
         // Vue 기본 설정. 
-        primary: '#1976D2',
+        primary: '#0d0e23',
         secondary: '#424242',
         accent: '#82B1FF',
         error: '#FF5252',
