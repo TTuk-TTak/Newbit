cea1a8596507d0a92ede510f5edb5e336b8af1c6
이수림 <tnfla9689@naver.com>
2022-02-17 17:51:56 +0900

    fix: CORS

--- backend/Newbit/src/main/java/com/ssafy/newbit/config/WebConfig.java
+++ backend/Newbit/src/main/java/com/ssafy/newbit/config/WebConfig.java
@@ -12,14 +12,9 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-//		System.out.println("CORS Setting");
-//		default 설정.
-//		Allow all origins.
-//		Allow "simple" methods GET, HEAD and POST.
-//		Allow all headers.
-//		Set max age to 1800 seconds (30 minutes).
 		registry.addMapping("/**")
-			.allowedOrigins("*")
+			.allowedOrigins("http://localhost:8080", "http://i6a101.p.ssafy.io:8080")
+			.allowedHeaders("*")
 			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 			.maxAge(6000)
 			.allowCredentials(true);
